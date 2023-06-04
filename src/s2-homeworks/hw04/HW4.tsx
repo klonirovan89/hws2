4c76a38170536088bb7c74c87e44b8e2551f3c24
klonirovan89 <klonirovan89@gmail.com>
2023-06-04 11:50:41 +0300

    Commit 3

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -9,16 +9,15 @@ import Stand from './Stand'
 * 4 - сделать стили в соответствии с дизайном
 * */
 
-const HW4 = () => {
+export const HW4 = () => {
     return (
         <div id={'hw4'}>
             <div className={s2.hwTitle}>Homework #4</div>
             {/*демонстрация возможностей компонент:*/}
-            <div className={s2.hw}>
+            <div className={`${s2.hw} ${s2.hw_4}`}>
                 <Stand />
             </div>
         </div>
     )
 }
-
-export default HW4
+export default HW4;
--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,18 +17,16 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    const finalClassName = `
+   ${s.button} ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default}
+   ${disabled ? s.disabled : ''} ${className ? ' ' + className : ''}
+   `.trim()
 
     return (
         <button
             disabled={disabled}
             className={finalClassName}
-            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+            {...restProps}
         />
     )
 }
